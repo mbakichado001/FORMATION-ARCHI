dd897d36b540f73715530c1e97318faba63627cf
tegra mbaki <mbakichado@gmail.com>
2023-08-08 14:13:36 +0200

    TP1 IMPOR ET EXPORT DES COMPOSANTS

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './Pages/Home';
+import { Audrey } from './Pages/Home';
+import { Albert } from './Pages/Home';
+import { Tegra } from './Pages/Home';
+import { Kanza } from './Pages/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Navbar/>
+    <ul>
+      <il>
+        <Audrey/>
+      </il>
+      <il>
+        <Albert/>
+      </il>
+      <il>
+        <Tegra/>
+      </il>
+      <il>
+        <Kanza/>
+      </il>
+    </ul>
+
+    </>
+    
+      
   );
 }
 
---
+++ src/Pages/Home.js
@@ -0,0 +1,79 @@
+function Parole() {
+
+    return(
+        <p>
+            nous avaons tous des reves, pourquoi c'est nous tous qui vivons cela? je pense que le mental est primordial.
+           <br/>LES PERSONNES QUI ONT VECU LEURS REVES :
+        </p>
+    )
+}
+
+export function Audrey() {
+    return(
+        <>
+
+         <h1>AUDREY KALL</h1>
+         <img 
+          src="https://i.imgur.com/MK3eW3As.jpg" 
+          alt="Audrey kall" 
+         />
+
+        </>
+        
+    )
+}
+
+export function Kanza() { 
+    return(
+        <>
+            <h1>CHADRACK KANZA</h1>
+            <img 
+            src=""
+            alt="Chadrack kanza"
+            />
+        
+        </>
+           
+    )
+}
+
+export function Tegra() {
+    return(
+        <>
+        <h1>TEGRA MBAKI</h1>
+        <img
+           src=""
+           alt="Tegra mbaki"
+        />
+        </>
+    )
+}
+
+export function Albert() {
+    return(
+        <>
+            <h1>ALBERT KONE</h1>
+            <img 
+            src="https://i.imgur.com/QIrZWGIs.jpg"
+            alt="Alan L. Hart"
+            />
+        </>
+        
+    )
+}
+
+
+
+
+
+
+
+export default function Navbar () {
+    return(
+        <>
+        <h1>Mon premier paragraphe</h1>
+        <Parole/>
+        </>
+        
+    )
+};
